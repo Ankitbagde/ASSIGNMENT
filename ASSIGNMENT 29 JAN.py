2f2b5ad346dc2a6825c92fb1f3b16a8c8e4f8d60
Ankitbagde <123974922+Ankitbagde@users.noreply.github.com>
2023-03-01 12:26:41 +0530

    Add files via upload

---
+++ ASSIGNMENT 29 JAN.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1. Who developed Python Programming Language');get_ipython().run_line_magic('pinfo', 'Language')
+
+
+# In[ ]:
+
+
+Python Programming Language was developed by Guido van Rossum.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('2. Which type of Programming does Python support');get_ipython().run_line_magic('pinfo', 'support')
+
+
+# In[ ]:
+
+
+Python supports Object-Oriented Programming, Procedural Programming, and Functional Programming paradigms.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('3. Is Python case sensitive when dealing with identifiers');get_ipython().run_line_magic('pinfo', 'identifiers')
+
+
+# In[ ]:
+
+
+Yes, Python is case sensitive when dealing with identifiers.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('4. What is the correct extension of the Python file');get_ipython().run_line_magic('pinfo', 'file')
+
+
+# In[ ]:
+
+
+The correct extension of the Python file is ".py".
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('5. Is Python code compiled or interpreted');get_ipython().run_line_magic('pinfo', 'interpreted')
+
+
+# In[ ]:
+
+
+Python code is interpreted, although it is first compiled into bytecode before interpretation
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('6. Name a few blocks of code used to define in Python language');get_ipython().run_line_magic('pinfo', 'language')
+
+
+# In[ ]:
+
+
+The blocks of code used to define in Python language are functions, classes, and modules.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('7. State a character used to give single-line comments in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+
+# In[ ]:
+
+
+The "#" symbol is used to give single-line comments in Python.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('8. Mention functions which can help us to find the version of python that we are currently working on');get_ipython().run_line_magic('pinfo', 'on')
+
+
+# In[ ]:
+
+
+Functions such as "sys.version" and "platform.python_version()" 
+can be used to find the version of Python that we are currently working on.
+
+
+# In[ ]:
+
+
+9. Python supports the creation of anonymous functions at runtime, using a construct called
+
+
+# In[ ]:
+
+
+Python supports the creation of anonymous functions at runtime, using a construct called "lambda" functions.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('10. What does pip stand for python? 11. Mention a few built-in functions in python');get_ipython().run_line_magic('pinfo', 'python')
+
+
+# In[ ]:
+
+
+Pip stands for "Pip Installs Packages" and is a package management system used to install 
+and manage software packages written in Python
+
+
+# In[ ]:
+
+
+11.Mention some built in function in python
+
+
+# In[ ]:
+
+
+Built-in functions in Python include "print()", "len()", "range()", "type()", "int()", "str()", "float()", "input()", and many more.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('12. What is the maximum possible length of an identifier in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+
+# In[ ]:
+
+
+The maximum possible length of an identifier in Python is unlimited.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('13. What are the benefits of using Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+
+# In[ ]:
+
+
+Benefits of using Python include its simplicity, ease of learning and use, 
+large community support, wide range of libraries and 
+frameworks, and versatility across multiple platforms and domains.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('14. How is memory managed in Python');get_ipython().run_line_magic('pinfo', 'Python')
+
+
+# In[ ]:
+
+
+Memory in Python is managed through a combination of reference counting and garbage collection. 
+When an object is no longer being referred to, its memory is automatically deallocated.
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('15. How to install Python on Windows and set path variables');get_ipython().run_line_magic('pinfo', 'variables')
+
+
+# In[ ]:
+
+
+To install Python on Windows, download the installer from the official Python website and run it. 
+During the installation process, make sure to check the 
+"Add Python to PATH" option to set the path variables automatically
+
+
+# In[ ]:
+
+
+get_ipython().set_next_input('16. Is indentation required in python');get_ipython().run_line_magic('pinfo', 'python')
+
+
+# In[ ]:
+
+
+Yes, indentation is required in Python, as it is used to denote blocks of code and is a 
+fundamental part of the language's syntax.
+
